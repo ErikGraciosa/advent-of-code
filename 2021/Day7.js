8c51bc892753d963f825f9edce9644dcacebe2de
Erik <erikgraciosa@gmail.com>
2022-04-11 18:26:09 -0700

    day7 part 2 solved

--- 2021/Day7.js
+++ 2021/Day7.js
@@ -20,8 +20,31 @@ for(let i = 0; i < horizontalLocations.length; i++) {
   fuelRequired = fuelRequired + Math.abs(horizontalLocations[i] - crabMedian);
 }
 
-console.log(fuelRequired);
+console.log(`Linear fuel cost calculated ${fuelRequired}`);
 
 //Answer too low 141695
 //.reduce fixed 328657 is too high
 //changed to a for loop 328318, right answer
+
+//PART 2
+
+const average = (arr) => {
+  return Math.round((arr.reduce((total, val) => total + val) / arr.length));
+}
+
+const linearFuelAdded = (distance) => {
+  return 0.5 * distance + 0.5 * distance * distance;
+}
+
+
+const crabAverage = average(horizontalLocations);
+let newFuelCalc = 0;
+for(let i = 0; i < horizontalLocations.length; i++) {
+  newFuelCalc = newFuelCalc + linearFuelAdded(Math.abs(horizontalLocations[i] - 467));
+}
+
+console.log(crabAverage)
+console.log(`Exponential fuel cost calculated ${newFuelCalc}`);
+//341134 is too low
+//89791190 is too high, rounded average 468
+//89791146 is right with rounded average 467
