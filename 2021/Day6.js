bff34e1ca15f56d4fc25d3a910e3ed5b3e0b6ee2
Erik <erikgraciosa@gmail.com>
2022-04-11 17:17:21 -0700

    day 6 part 2 histogram solution

--- 2021/Day6.js
+++ 2021/Day6.js
@@ -7,11 +7,12 @@ const inputData = readFile.split(',');
 
 //Following the laternfish repoduction rules how many laternfish exist after 80 days.
 
-const DAYS = 80;
+const DAYS = 256;
 
 //Initial state
 //Array that represents fish.
 const laternfish = inputData.map(num => Number(num));
+let laternfishPart2 = inputData.map(num => Number(num));
 //Need to process a day
 //If fish goes to 0 then push an '8' to the end of the array
 const processOneDay = (array) => {
@@ -34,11 +35,11 @@ const processOneDay = (array) => {
   return array;
 }
 
-for(let i = 0; i < DAYS; i++) {
-  processOneDay(laternfish);
-}
+//Not good for large arrays
+// for(let i = 0; i < DAYS; i++) {
+//   processOneDay(laternfish);
+// }
 
-console.log(laternfish.length)
 //Guess of 3300 too low
 //377263 right answer
 
@@ -46,4 +47,34 @@ console.log(laternfish.length)
 //Has issues with # Fatal error in , line 0
 //# Fatal JavaScript invalid size error 144230630
 
+//Is there a calculation for how many fish in the future will spawn for days to simulate?
 
+//Set up for a fish histogram and life cycle
+//Initial array
+const fishPopulationByDay = [0, 0, 0, 0, 0, 0, 0, 0, 0];
+
+//Distribute the fish
+laternfishPart2.forEach(fish => {
+  fishPopulationByDay[fish]++;
+});
+
+//Array mutating function
+const processHistogram = (array) => {
+  //Save the day 0 spawning fish
+  const spawningFish = array[0];
+  
+  //Age all fish
+  array.shift();
+
+  //Add the spawning fish to 6-index and 8-index
+  array[6] = array[6] + spawningFish;
+  array[8] = spawningFish;
+
+  return array;
+}
+
+for(let i = 0; i < DAYS; i++) {
+  processHistogram(fishPopulationByDay);
+}
+
+console.log(fishPopulationByDay.reduce((val, total) => val + total));
