0fe5c3d7307fd75949ac20a0ec842b641d82f959
erikgraciosa <erikgraciosa@gmail.com>
2024-12-05 14:08:15 -0800

    day2-2024 broken part 2

--- 2024/day2/Day2.java
+++ 2024/day2/Day2.java
@@ -3,7 +3,9 @@ package day2;
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Paths;
+import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.Collections;
 import java.util.List;
 import java.util.stream.Collectors;
 
@@ -71,20 +73,37 @@ public class Day2 {
                 if(safeListIncreasing == false || safeListDecreasing == false || listOfLists.get(i).get(0) == listOfLists.get(i).get(1)) {
                     badCounter++;
                 }
-                // Answer 472
-                System.out.println(listOfLists.size() - badCounter);
-
+                
+                
+                
+            }
+            // Answer 472
+            System.out.println(listOfLists.size() - badCounter);
 
             //Part 2
             // Allow a single 'bad level' (non decreasing or non increasing value or beyond 3 delta)
-            int badCounter = 0;
+                // 
+            part2(listOfLists);
+     
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        
+
+
+    }
+
+
+    public static int part1(List<List<Integer>> listOfLists) {
+        int badCounter = 0;
 
             for (int i = 0; i < listOfLists.size(); i++) {
                 boolean safeListIncreasing = true;
                 boolean safeListDecreasing = true;
                 for (int j = 0; j < listOfLists.get(i).size() - 1; j++) {
                     //Increasing scenario
-                    if(listOfLists.get(i).get(0) < listOfLists.get(i).get(listOfLists.get(i).size())) {
+                    if(listOfLists.get(i).get(0) < listOfLists.get(i).get(1)) {
                         if (listOfLists.get(i).get(j) < listOfLists.get(i).get(j + 1) &&
                         listOfLists.get(i).get(j) + 3 >= listOfLists.get(i).get(j + 1)) {
                             //Do nothing
@@ -93,7 +112,7 @@ public class Day2 {
                         }
                     }
                     //Decreasing scenario
-                    if(listOfLists.get(i).get(0) > listOfLists.get(i).get(listOfLists.get(i).size())) {
+                    if(listOfLists.get(i).get(0) > listOfLists.get(i).get(1)) {
                         if (listOfLists.get(i).get(j) > listOfLists.get(i).get(j + 1) &&
                         listOfLists.get(i).get(j) - 3 <= listOfLists.get(i).get(j + 1)) {
                             //Do nothing
@@ -106,14 +125,86 @@ public class Day2 {
                     badCounter++;
                 }
                 // Answer 472
-                System.out.println(listOfLists.size() - badCounter);
-
+                //System.out.println(listOfLists.size() - badCounter);
 
 
             }
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
+            return badCounter;
+    }
+    
+    public static void part2(List<List<Integer>> listOfLists) {
+        int badCounter = 0;
+        List<List<Integer>> badList = new ArrayList<>();
 
+        for (int i = 500; i < listOfLists.size() - 1; i++) {
+            boolean safeListIncreasing = true;
+            boolean safeListDecreasing = true;
+            boolean equalFirstNumbers = true;
+            System.out.println("this is the input array: " + listOfLists.get(i));
+            for (int j = 0; j < listOfLists.get(i).size() - 1;) {
+                //Equals scenario
+                if(listOfLists.get(i).get(0) == listOfLists.get(i).get(1)) {
+                    List<Integer> workerList = new ArrayList<>(listOfLists.get(i));
+                        System.out.println(listOfLists.get(i));
+                        workerList.remove(j + 1);
+                        System.out.println(workerList);
+                        badList.add(workerList);
+                        equalFirstNumbers = false;
+                        j = 10000;
+                } else if(listOfLists.get(i).get(0) < listOfLists.get(i).get(listOfLists.get(i).size() - 1)) {
+                    if (listOfLists.get(i).get(j) < listOfLists.get(i).get(j) &&
+                    listOfLists.get(i).get(j) + 3 >= listOfLists.get(i).get(j + 1)) {
+                        //Do nothing
+                        System.out.println("valid increment" + listOfLists.get(i).get(j) + " : " +  listOfLists.get(i).get(j + 1));
+                        j++;
+                    } else {
+                        //Remove the bad element and add to badList for reprocess.
+                        List<Integer> workerList = new ArrayList<>(listOfLists.get(i));
+                        workerList.remove(j + 1);
+                        badList.add(workerList);
+                        safeListIncreasing = false;
+                        j = 10000;
+                    }
+                } else if(listOfLists.get(i).get(0) > listOfLists.get(i).get(listOfLists.get(i).size() - 1)) {
+                    if (listOfLists.get(i).get(j) > listOfLists.get(i).get(j + 1) &&
+                    listOfLists.get(i).get(j) - 3 <= listOfLists.get(i).get(j + 1)) {
+                        System.out.println("valid decrement" + listOfLists.get(i).get(j) + " : " +  listOfLists.get(i).get(j + 1));
+
+                        //Do nothing
+                        j++;
+                    } else {
+                        List<Integer> workerList = new ArrayList<>(listOfLists.get(i));
+                        System.out.println(listOfLists.get(i));
+                        workerList.remove(j + 1);
+                        System.out.println(workerList);
+                        badList.add(workerList);
+                        safeListDecreasing = false;
+                        j = 10000;
+                    }
+                } else if(listOfLists.get(i).get(0) == listOfLists.get(i).get(listOfLists.get(i).size() - 1)) {
+                    List<Integer> workerList = new ArrayList<>(listOfLists.get(i));
+                        System.out.println(listOfLists.get(i));
+                        workerList.remove(j + 1);
+                        System.out.println(workerList);
+                        badList.add(workerList);
+                        safeListDecreasing = false;
+                        j = 10000;
+                } else {
+                    String message = "null case hit";
+                    System.out.println(message);
+                }
+                
+
+            }
+
+        int part2failures = part1(badList);
+
+        // if(safeListIncreasing == false || safeListDecreasing == false || equalFirstNumbers == false) {
+        //     badCounter++;
+        // }
+        // Answer 472
+            
+        System.out.println(listOfLists.size() - part2failures);
+        }
     }
 }
